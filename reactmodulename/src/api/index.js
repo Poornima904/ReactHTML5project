177b16cd777eb5fa2024446869b9dac6ca82747b
Poornima A M <poornima.am@peolsolutions.com>
2024-09-17 05:35:50 +0000

    ini commit

---
+++ reactmodulename/src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseURL = "react_url/";
+
+const instance = axios.create({
+  baseURL
+});
+
+export const getTableData = async (params = { $top: 100, $skip: 0 }) => {
+  const { data } = await instance.get("/Books", {
+    params
+  });
+
+  return data.d?.results || data.d || data.value;
+};
+
+export const getTableCount = async () => {
+  const { data } = await instance.get("/Books/$count");
+  return data;
+};
